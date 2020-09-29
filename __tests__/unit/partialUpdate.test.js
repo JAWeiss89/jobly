98b2fbe6bb91178ba7d014614b23371dcb35ebc7
JAWeiss89 <jorge.a.weiss@gmail.com>
2020-09-29 14:33:52 -0500

    Add tests for helper function

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,22 @@
-describe("partialUpdate()", () => {
+const sqlForPartialUpdate = require("../../helpers/partialUpdate");
+
+describe("sqlForPartialUpdate()", () => {
   it("should generate a proper partial update query with just 1 field",
       function () {
 
     // FIXME: write real tests!
-    expect(false).toEqual(true);
+    // partialUpdate (table, items, key, id)
+    let table = "cats";
+    let items = {"name": "Jorge", "age": 6};
+    let key = "id";
+    let id = "5";
+
+    let expectedQuery = "UPDATE cats SET name=$1, age=$2 WHERE id=$3 RETURNING *";
+
+    expect(sqlForPartialUpdate(table, items, key, id).query).toEqual(expectedQuery);
+    expect(sqlForPartialUpdate(table, items, key, 5).query).toEqual(expectedQuery);
+    expect(sqlForPartialUpdate(table, items, key, 5).values).toEqual(["Jorge", 6, 5]);
+
 
   });
 });
